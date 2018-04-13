3f8d19b41382312e3bf367a29e12f7435443e897
patrick brisbin <pbrisbin@gmail.com>
2018-04-13 11:54:43 -0400

    Progress on some test cases

--- src/ShellWords.hs
+++ src/ShellWords.hs
@@ -1,11 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module ShellWords
     ( parse
     ) where
 
+--import Control.Monad (void)
+import Data.Char
+import Data.Semigroup ((<>))
 import Data.Text (Text)
-import qualified Data.Text as T
+import Data.Text as T
+import Text.Megaparsec hiding (parse)
+import qualified Text.Megaparsec as Megaparsec
+import Text.Megaparsec.Char
 
-type ParseError = Text
+type Parser = Parsec Text Text
 
-parse :: Text -> Either ParseError [Text]
-parse = Right . T.words
+parse :: Text -> Either (ParseError Char Text) [Text]
+parse = Megaparsec.parse parser "<input>"
+
+parser :: Parser [Text]
+parser = shellword `sepBy` space1
+
+shellword :: Parser Text
+shellword = shelloption <|> shellargument
+
+shelloption :: Parser Text
+shelloption = string "nope"
+
+shellargument :: Parser Text
+shellargument = quoted anyToken <|> unquotedValue
+
+quoted :: Parser Char -> Parser Text
+quoted p = do
+    q <- oneOf ['\'', '\"']
+    T.pack <$> manyTill (try (escaped q) <|> p) (char q)
+
+unquotedValue :: Parser Text
+unquotedValue = T.pack <$> many (try (escaped ' ') <|> nonSpace)
+
+escaped :: Char -> Parser Char
+escaped c = c <$ string ("\\" <> T.singleton c)
+
+nonSpace :: Parser Char
+nonSpace = satisfy $ not . isSpace
+
+anyToken :: Parser Char
+anyToken = satisfy $ const True
