0ce54d0fe00d43126763bf180bfb00df50729d7f
patrick brisbin <pbrisbin@gmail.com>
2024-11-05 07:05:53 -0500

    Migrate to fourmolu

--- src/Text/Megaparsec/Compat.hs
+++ src/Text/Megaparsec/Compat.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE CPP #-}
 
 module Text.Megaparsec.Compat
-    ( module X
-#if !MIN_VERSION_megaparsec(7,0,0)
-    , errorBundlePretty
-#endif
-    ) where
+  ( module X
+  , errorBundlePretty
+  ) where
+
+#if MIN_VERSION_megaparsec(7,0,0)
+-- Import errorBundlePretty not as part of X, so it can be exported separately
+-- as is necessary in the < 7.0.0 case
+import Text.Megaparsec as X hiding (errorBundlePretty)
+import Text.Megaparsec (errorBundlePretty)
+#else
+import Prelude
 
 import Text.Megaparsec as X
-
-#if !MIN_VERSION_megaparsec(7,0,0)
-import Prelude
 
 errorBundlePretty :: Show a => a -> String
 errorBundlePretty = show
